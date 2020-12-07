922aa087b12c68c45df76bfe1f2cbb5f199fdf57
israelias <israelias.js@gmail.com>
2020-12-07 23:10:04 +0800

    Configure layout template and css module

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,9 +1,12 @@
-import Link from 'next/link'
 import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../../components/layout'
+
+
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
             <Head>
                 <title>First Post</title>
             </Head>
@@ -13,6 +16,6 @@ export default function FirstPost() {
                     <a>Back to home</a>
                 </Link>
             </h2>
-        </>
+        </Layout>
     )
 }
